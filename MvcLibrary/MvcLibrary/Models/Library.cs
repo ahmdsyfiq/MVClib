d5cf27b10fe64a2c64f19c8502d8cc06b03fb052
unknown <ahmdsyfiq@gmail.com>
2023-08-31 12:49:36 +0700

    file VS

---
+++ MvcLibrary/MvcLibrary/Models/Library.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcLibrary.Models;
+
+public class Library
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? Publisher { get; set; }
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+}
---
+++ MvcLibrary/MvcLibrary/Models/SeedData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcLibrary.Data;
+using System;
+using System.Linq;
+
+namespace MvcLibrary.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcLibraryContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcLibraryContext>>()))
+        {
+            // Look for any movies.
+            if (context.Library.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Library.AddRange(
+                new Library
+                {
+                    Title = "Kambing Jantan",
+                    Author = "Raditya Dika",
+                    Publisher = "Gramedia",
+                    ReleaseDate = DateTime.Parse("2003-2-12"),
+                    Genre = "Comedy",
+                    Price = 15
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
